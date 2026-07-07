a3eedf0934c3f7b9467090497b41e61319f5d943
rahul888712345 <raorahulrr33@gmail.com>
2026-07-07 23:25:57 +0530

    change

--- ReactApp/RD_SysTem/src/App.jsx
+++ ReactApp/RD_SysTem/src/App.jsx
@@ -1,20 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./component/Navbar.jsx";
+import NavScroll from "./component/NavScroll.jsx";
 import Home from "./component/Home.jsx";
 import About from "./component/About.jsx";
-import Login from "./Login.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       {/* Components placed here (like a Navbar) stay visible on every single page */}
-      <Navbar />
+      <NavScroll />
 
       {/* The Router switches out the components inside this block based on the URL */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- ReactApp/RD_SysTem/src/component/NavScroll.jsx
+++ ReactApp/RD_SysTem/src/component/NavScroll.jsx
@@ -4,13 +4,14 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Servicess from "./../../../../RD_System/src/component/Services";
 
-export default function Nav() {
+export default function NavScroll() {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="#">RD System</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -18,21 +19,16 @@ export default function Nav() {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#action6">Servicess</Nav.Link>
+
+              <NavDropdown title="User" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Settings</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action5">LogOut</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
             </Nav>
             <Form className="d-flex">
               <Form.Control
