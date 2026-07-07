02143cb2106ab4a52ed3165e81a2560ec19a6743
rahul888712345 <raorahulrr33@gmail.com>
2026-07-07 22:56:37 +0530

    Change

--- ReactApp/RD_SysTem/src/App.jsx
+++ ReactApp/RD_SysTem/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar.jsx";
+import Home from "./component/Home.jsx";
+import About from "./component/About.jsx";
+import Login from "./Login.jsx";
+
 export default function App() {
   return (
-    <>
-      <h1>Welcome to React App</h1>
-    </>
+    <BrowserRouter>
+      {/* Components placed here (like a Navbar) stay visible on every single page */}
+      <Navbar />
+
+      {/* The Router switches out the components inside this block based on the URL */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
