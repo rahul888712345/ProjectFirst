7c7e3198aaa271e11f0fb41c74c2485b2d1940d4
rahul888712345 <raorahulrr33@gmail.com>
2026-07-07 23:03:44 +0530

    change

--- ReactApp/RD_SysTem/src/component/Nav.jsx
+++ ReactApp/RD_SysTem/src/component/Nav.jsx
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-export default function Navbar() {
+export default function Nav() {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
