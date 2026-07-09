e523cd7cf164bb253ab9efc1317e7a722fc76155
rahul888712345 <raorahulrr33@gmail.com>
2026-07-09 16:44:23 +0530

    change

--- RDSystem/rdsystem/src/Contact.jsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <>
-      <h1>Welcome to Contact</h1>
-    </>
-  );
-}
--- RDSystem/rdsystem/src/Servicess.jsx
+++
@@ -1,7 +0,0 @@
-export default function Servicess() {
-  return (
-    <>
-      <h1>Welcome to Servicess</h1>
-    </>
-  );
-}
